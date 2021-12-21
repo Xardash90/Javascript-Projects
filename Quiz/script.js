575fe73e980094b37161d91f2fc4682eb4380d08
Xardash90 <92758949+Xardash90@users.noreply.github.com>
2021-12-22 00:10:09 +0100

    Add files via upload

--- Quiz/script.js
+++ Quiz/script.js
@@ -36,28 +36,29 @@ const quizData = [
 const quiz = document.getElementById("quiz");
 const answerEls = document.querySelectorAll(".answer");
 const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
 
-const a_text = document.getElementById('a_text');
-const b_text = document.getElementById('b_text');
-const c_text = document.getElementById('c_text');
-const d_text = document.getElementById('d_text');
 let currentQuiz = 0;
 let score = 0;
 
 loadQuiz();
 
 function loadQuiz() {
-	const currentQuizData = quizData[currentQuiz];
-	
-	questionEl.innerText = currentQuizData.question;
-	a_text.innerText = currentQuizData.a;
-	b_text.innerText = currentQuizData.b;
-	c_text.innerText = currentQuizData.c;
-	d_text.innerText = currentQuizData.d;
-	
+    deselectAnswers();
 
+    const currentQuizData = quizData[currentQuiz];
 
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
 }
+
 function getSelected() {
     let answer = undefined;
 
@@ -75,7 +76,6 @@ function deselectAnswers() {
         answerEl.checked = false;
     });
 }
-
 
 submitBtn.addEventListener("click", () => {
     // check to see the answer
@@ -98,8 +98,3 @@ submitBtn.addEventListener("click", () => {
         }
     }
 });
-
-		
-
-
-
