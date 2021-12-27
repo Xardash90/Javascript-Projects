15e02c72af33f8c61dc87bdb321074e7b202f638
Xardash90 <92758949+Xardash90@users.noreply.github.com>
2021-12-27 11:44:27 +0100

    Add files via upload

--- MovieApp/script.js
+++ MovieApp/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	
 
 const APIURL =
-    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1&language=de";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
     "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
