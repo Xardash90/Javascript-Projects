bf7877f908f3666fc35ccd063119f37f19655df1
Xardash90 <92758949+Xardash90@users.noreply.github.com>
2021-12-27 13:31:53 +0100

    Add files via upload

--- MovieApp/script.js
+++ MovieApp/script.js
@@ -2,18 +2,26 @@
 document.addEventListener("DOMContentLoaded", () => {
 	
 
-const APIURL =
-    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1&language=de";
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1&language=de";
+const APIURL2 = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&language=de&page=";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI =
-    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+const LANGAPI = "&language=de";
+let nextPageCounter = 1;
+
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
+const nextButton = document.getElementById("nextSite");
+const lastButton = document.getElementById("lastSite");
+const bottomNextButton = document.getElementById("bottomNextSite");
+const bottomLastButton = document.getElementById("bottomLastSite");
 
 // initially get fav movies
 getMovies(APIURL);
+HeaderButtons();
+BottomButtons();
 
 async function getMovies(url) {
     const resp = await fetch(url);
@@ -23,6 +31,8 @@ async function getMovies(url) {
 
     showMovies(respData.results);
 }
+
+
 
 function showMovies(movies) {
     // clear main
@@ -71,11 +81,58 @@ form.addEventListener("submit", (e) => {
     const searchTerm = search.value;
 
     if (searchTerm) {
-        getMovies(SEARCHAPI + searchTerm);
+        getMovies(SEARCHAPI + searchTerm + LANGAPI);
 
-        search.value = "";
+    
     }
+	else if (searchTerm == "") {
+		getMovies(APIURL);
+	}
 });
 
 
-});
+function HeaderButtons() {		
+nextButton.addEventListener("click", (e) => {
+	e.preventDefault();
+	nextPageCounter++;
+	getMovies(APIURL2 + nextPageCounter);
+	console.log(nextPageCounter);
+	
+})
+lastButton.addEventListener("click", (e) => {
+	e.preventDefault();
+	if(nextPageCounter == 1){
+		nextPageCounter = 1;
+		getMovies(APIURL2 + nextPageCounter);	
+	} else {
+		nextPageCounter--;
+		getMovies(APIURL2 + nextPageCounter);
+	  }	
+	 })
+
+}
+
+function BottomButtons() {		
+bottomNextButton.addEventListener("click", (e) => {
+	e.preventDefault();
+	nextPageCounter++;
+	getMovies(APIURL2 + nextPageCounter);
+	console.log(nextPageCounter);
+	
+})
+bottomLastButton.addEventListener("click", (e) => {
+	e.preventDefault();
+	if(nextPageCounter == 1){
+		nextPageCounter = 1;
+		getMovies(APIURL2 + nextPageCounter);	
+	} else {
+		nextPageCounter--;
+		getMovies(APIURL2 + nextPageCounter);
+	  }	
+	 })
+
+}
+
+
+
+})
