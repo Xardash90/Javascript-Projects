2bdf4e6e7a9fd99c9af8b755e46c694f65f655e2
Xardash90 <92758949+Xardash90@users.noreply.github.com>
2021-12-29 16:12:15 +0100

    Add files via upload

--- CryptoApp/script.js
+++ CryptoApp/script.js
@@ -2,43 +2,47 @@
 
 const APIURL = "https://api.coincap.io/v2/assets";
 
-const main = document.getElementById("main")
+const mains = document.getElementById("main");
 
+console.log(mains);
 
 getCryptoData(APIURL);
 
 async function getCryptoData(url){
-    const resp = await fetch(url);
+       const resp = await fetch(url);
     const respData = await resp.json();
 	
 	const coinName = respData.data[0].name;
 	
-	console.log(respData);
-	showCryptoData(respData.results);
+
+	showCryptoData(respData.data);
 }
 
 
 async function showCryptoData(coins) {
 
 
+
 	coins.forEach((coin) => {
-	const { id, symbol, name, priceUsd, changePercent24Hr } = coin;	
+	const {  name, priceUsd, changePercent24Hr  } = coin;	
 	
+
 	    const coinEl = document.createElement("div");
         coinEl.classList.add("card-container");
 
         coinEl.innerHTML = `
-           <div class="card-header">
-						<img src="crypto.png" />
-						<h3>Bitcoin</h3>				
+          
+					<div class="card-header">
+						
+						<h3>${name}</h3>				
 					</div>
-						<div class="card-body">
-							<small>47700,00 $</small>
-							<small>6.6%</small>
+				<div class="card-body">
+							<small>${priceUsd}</small>
+							<small>${changePercent24Hr}</small>
 							<i class="fas fa-chart-bar"></i>
 						</div>
         `;
-		console.log(coinEl);
+
 
         main.appendChild(coinEl);
 	
