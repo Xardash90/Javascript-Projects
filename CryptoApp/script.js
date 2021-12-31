2ac5a17f0fca775d151fb1cf2df71ac7e5558ec2
Xardash90 <92758949+Xardash90@users.noreply.github.com>
2021-12-31 12:04:22 +0100

    Add files via upload

--- CryptoApp/script.js
+++ CryptoApp/script.js
@@ -1,28 +1,31 @@
-
+document.addEventListener("DOMContentLoaded", () => {
+	
 const APIKEY = "ec4c7048-55d6-495a-bbdc-dcd2f7cadd23";
 const APIURL = "https://api.coincap.io/v2/assets/";
+const SEARCHAPI = "https://api.coincap.io/v2/assets/";
 
-const mains = document.getElementById("main");
+
+const form = document.getElementById("form");
+
 
 
 
 getCryptoData(APIURL);
 
 async function getCryptoData(url){
-       const resp = await fetch(url);
+    const resp = await fetch(url);
     const respData = await resp.json();
 	
-	const coinName = respData.data[0].name;
-	
-
 	showCryptoData(respData.data);
+	
 }
 
 
 
-async function showCryptoData(coins) {
 
-		
+async function showCryptoData(coins) {	
+	main.innerHTML = "";
+	
 	coins.forEach((coin) => {
 		
 	const { rank, symbol, name, priceUsd, changePercent24Hr  } = coin;	
@@ -34,14 +37,14 @@ async function showCryptoData(coins) {
 	let icon =	document.getElementById('icon');
 	// icon.add.
 	// icon.style.backgroundColor = "yellow";
-	console.log(icon)
+	// console.log(icon)
 	}
 	
 	    const coinEl = document.createElement("div");
         coinEl.classList.add("card-container");
 
         coinEl.innerHTML = `
-				
+          
 					<div class="card-header">
 					<small class="rank"><i id="icon" class="fa fa-star"  style="font-size:15px;color:"></i>   Rank: ${rank}</small> 
 						<h3>${name}</h3>				
@@ -51,15 +54,26 @@ async function showCryptoData(coins) {
 				
 							<i class="fas fa-chart-bar"></i>
 						</div>
-						
-	
         `;
-
-
         main.appendChild(coinEl);
-	
+
    });
 }
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+		const searchTerm = search.value;
+
+		if (searchTerm) {
+			getCryptoData(SEARCHAPI + searchTerm);
+
+		console.log(SEARCHAPI + searchTerm);
+		}
+		else if (searchTerm == "") {
+			getCryptoData(APIURL);
+		}
+	});
 
 // function hightlightPercentage(percent) {
 		
@@ -74,7 +88,7 @@ async function showCryptoData(coins) {
 
 
 
-
+})
 
 
 
