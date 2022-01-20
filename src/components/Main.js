cd5ab017218d575a9f0fe09e990c0ff4535cd9be
Xardash90 <92758949+Xardash90@users.noreply.github.com>
2022-01-20 13:09:30 +0100

    Add files via upload

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+export default function Main() {
+    return (
+        <main>
+            <h1 className="main--title">Fun Facts about React</h1>
+            <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Created By Jordan Walke</li>
+                <li>Has 100k Stars on Github</li>
+                <li>Its maintened by Facebook</li>
+                <li>Powers thousands of enterprise Apps</li>
+            </ul> 
+        </main>
+    )
+}
+
