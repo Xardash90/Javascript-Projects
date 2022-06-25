cfea7eab2a64e78a1530585043691d078dcc3acf
Xardash90 <92758949+Xardash90@users.noreply.github.com>
2022-06-25 17:58:03 +0200

    Delete 01_shipping_lanes directory

--- 01_shipping_lanes/clock.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo '&nbsp;<small>Aktualisierung&nbsp;'.date('d.m.Y H:i:s').'&nbsp;</small>&nbsp;&nbsp;&nbsp;' ; //letzte&nbsp;
-
-?>
--- 01_shipping_lanes/reset_color.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-include ('../../login/conf.php');
-				
-mysql_query("UPDATE `wi_reports`.`outbound_gate` SET `O_COLOR`='#F2F2F2;color:#717171' WHERE `O_MANDAT` = '".$_SESSION["location"]."' ");
-		 						
-mysql_close($conn);
-?>
